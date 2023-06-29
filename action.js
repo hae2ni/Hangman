6fab496116961051a09ef49147706cf71b87b9b8
HAE2NI <hae0_02ni@ewhain.net>
2023-06-29 14:44:04 +0900

    ✨ 행맨 그리기

--- action.js
+++ action.js
@@ -3,11 +3,13 @@ const answer = word.split("");
 let life = answer.length;
 
 let wrong = [];
+let youWrote = [];
 
 const $answerInput = document.getElementById("anwerInput");
 const $youwrong = document.getElementById("youwrong");
 const $lifeCount = document.getElementById("lifeCount");
 const $underLine = document.querySelectorAll(".underLine");
+const $isSame = document.getElementById("isSame");
 
 const $g = document.querySelector(".g");
 const $o = document.querySelector(".o");
@@ -18,9 +20,23 @@ const $u = document.querySelector(".u");
 
 const $answer = document.querySelectorAll(".answer ");
 
-//글자 입력 함수
-function inputAnswer() {
+const $canvas = document.getElementById("hangman");
+const context = $canvas.getContext("2d");
+const DRAWS = ["head", "body", "rightHarm", "leftHarm", "rightLeg", "leftLeg"];
+
+//클릭해서 틀린 애들 화면에 불러오기
+let step = 0;
+let input = "";
+function clickBtn() {
   const $answervalue = $answerInput.value;
+  input = $answervalue;
+  const isSame = document.createElement("p");
+  $isSame.appendChild(isSame);
+  if (youWrote.includes($answervalue)) {
+    setTimeout(() => {
+      isSame.innerText += "이미 있는 단어입니다.";
+    }, [3000]);
+  }
 
   $answer.forEach((value) =>
     value.classList.contains($answervalue)
@@ -32,13 +48,72 @@ function inputAnswer() {
         )
       : wrong.push($answervalue)
   );
-}
+  youWrote.push($answervalue);
 
-//클릭해서 틀린 애들 화면에 불러오기
-function clickBtn() {
-  console.log(wrong);
-  answer.includes(wrong[0]) ? undefined : ($youwrong.innerText += wrong[0]);
+  answer.includes(wrong[0])
+    ? undefined
+    : ($youwrong.innerText += wrong[0]) & Draw(DRAWS[step++]);
   wrong = [];
+
+  if (step === 6) {
+    isSame.innerText += "힝,,,나 주거뗘ㅠ";
+  }
+
+  $answerInput.value = " ";
 }
 
-function reset() {}
+function reset() {
+  context.clearRect(0, 0, $canvas.clientWidth, $canvas.height);
+}
+
+//행맨그리기
+function Draw(part) {
+  switch (part) {
+    case "head":
+      context.lineWidth = 5;
+      context.beginPath();
+      context.arc(100, 50, 25, 0, Math.PI * 2, true);
+      context.closePath();
+      context.stroke();
+      break;
+
+    case "body":
+      context.beginPath();
+      context.moveTo(100, 75);
+      context.lineTo(100, 140);
+      context.stroke();
+      break;
+
+    case "rightHarm":
+      context.beginPath();
+      context.moveTo(100, 85);
+      context.lineTo(60, 100);
+      context.stroke();
+      break;
+
+    case "leftHarm":
+      context.beginPath();
+      context.moveTo(100, 85);
+      context.lineTo(140, 100);
+      context.stroke();
+      break;
+
+    case "leftLeg":
+      context.beginPath();
+      context.moveTo(100, 140);
+      context.lineTo(125, 190);
+      context.stroke();
+      break;
+
+    case "rightLeg":
+      context.beginPath();
+      context.moveTo(100, 140);
+      context.lineTo(80, 190);
+      context.stroke();
+      break;
+
+    default:
+      return;
+      break;
+  }
+}
